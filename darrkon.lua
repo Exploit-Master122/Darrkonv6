1b00fc3671a003371415f58ac042d185a556f5dc
Exploit_Master12 <viaangothivrekar@gmail.com>
2025-08-30 18:59:14 -0700

    Create darrkon.lua

---
+++ darrkon.lua
@@ -0,0 +1,10 @@
+Go to:
+https://darrkonroblox.vercel.app/
+
+For r0blox on vercel unbl0ck3d with the Now.GG API! 
+
+If not,then try running 
+
+https://web.cloudmoonapp.com/game/com.roblox.client/
+in the pr0xy using IframeVault,in Darrkon.
+- Exploit-Master122
